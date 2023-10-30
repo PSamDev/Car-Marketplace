490d5107b3ba5cf728328cc3454e6af897be629c
Wilfred Samuel <wilfredsamuel000@gmail.com>
2023-10-30 12:46:36 +0100

    Template files added

--- Automax/AutomaxApp/views.py
+++ Automax/AutomaxApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("<h1>Welcome!</h1>")
+    return render(request, "welcome.html", {"name":"AutoMax"})
