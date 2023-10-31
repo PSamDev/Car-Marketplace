dfdf832bfc3844fab4fc6bd39949e2f7a76147d5
Wilfred Samuel <wilfredsamuel000@gmail.com>
2023-10-31 23:20:09 +0100

    User Profile Model Defined

--- Automax/userapp/models.py
+++ Automax/userapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(null=True, blank=True, upload_to=None, height_field=None, width_field=None, max_length=None)
+    bio = models.CharField(max_length=500, blank=True, null=True)
+    phone_number = models.CharField(max_length=11, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user}'s Profile"
+    
