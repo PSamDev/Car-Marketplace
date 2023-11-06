fc191ce35d199f470228d09b4a3f647dc4ed4bdc
Wilfred Samuel <wilfredsamuel000@gmail.com>
2023-11-06 13:41:22 +0100

    Signal functionalities added

--- Automax/AutomaxApp/signals.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from userapp.models import Profile
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-def create_user_profile(sender, instance, created, **kwargs):
-    pass
