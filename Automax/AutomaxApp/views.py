5e846d135b5367bcae4ab671e1cfc89df4cfcab2
Wilfred Samuel <wilfredsamuel000@gmail.com>
2023-11-16 23:19:53 +0100

    Added gitignore file

--- Automax/AutomaxApp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def welcome(request):
-    return render(request, "welcome.html", {"name":"AutoMax"})
