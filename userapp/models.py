ac3700e984f90ac7ce5e98f366c04737183d2f62
Wilfred Samuel <wilfredsamuel000@gmail.com>
2023-11-16 23:20:44 +0100

    Updates

---
+++ userapp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#Download django localflavor to have access to autodetection of states, zip_code etc. Load on the settings.py file too
+# from localflavor.us.models import USPostalCodeField, USStateField, USSocialSecurityNumberField, USZipCodeField
+
+# Create your models here.
+    # Working with countries that are available on the django localflavor
+    # state = USStateField(default = "NY")
+    # zip_code = USZipCodeField(default="NY")
+
+   
+from django.db import models
+from django.contrib.auth.models import User
+
+class Location(models.Model):
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)  
+    zip_code = models.CharField(max_length=10)  
+    address = models.CharField(max_length=100) 
+
+    def __str__(self):
+        return f"{self.state}"
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/')  # Set upload_to directory
+    bio = models.CharField(max_length=500, blank=True, null=True)
+    phone_number = models.CharField(max_length=11, blank=True, null=True)
+    location = models.OneToOneField(Location, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user}'s Profile"
+
+    
---
+++ userapp/signals.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from .models import Profile, Location
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
+
+@receiver(post_save, sender = Profile)
+def create_profile_location(sender, instance, created, **kwargs):
+    if created:
+        profile_location = Location.objects.create()
+        instance.location = profile_location
+        instance.save()
