35ee6580ecd0fce0ab8f53ddb6c08b711a059d2d
Wilfred Samuel <wilfredsamuel000@gmail.com>
2023-11-18 23:57:32 +0100

    Login Form

--- userapp/views.py
+++ userapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login_view(request):
-    login_form = AuthenticationForm
+    if request.method == "POST":
+        login_form = AuthenticationForm(request=request, data=request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get("username")
+            password = login_form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            print(user)
+    elif request.method == "GET":
+        login_form = AuthenticationForm()
+        
     return render(request, "login.html", {"login_form":login_form})
