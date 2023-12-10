544c8457dc795891ccb068e61bffd347ce771f12
Wilfred Samuel <wilfredsamuel000@gmail.com>
2023-12-10 23:21:35 +0100

    Listing

--- AutomaxApp/models.py
+++ AutomaxApp/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
+from userapp . models import Profile
 
 # Create your models here.
+class Listing(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now_add=True)
+    seller = models.ForeignKey(Profile, on_delete=models.CASCADE)
