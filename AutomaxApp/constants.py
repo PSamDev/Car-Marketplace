31a54c80f049715bf79930b82885c00ba7e6a32d
Wilfred Samuel <wilfredsamuel000@gmail.com>
2023-12-11 11:08:26 +0100

    Listing Model

---
+++ AutomaxApp/constants.py
@@ -0,0 +1,25 @@
+CAR_BRANDS = (
+    ('bmw', 'BMW'),
+    ('mercedes benz', 'Mercedes Benz'),
+    ('audi', 'Audi'),
+    ('subaru', 'Subaru'),
+    ('tesla', 'Tesla'),
+    ('jaguar', 'Jaguar'),
+    ('land rover', 'Land Rover'),
+    ('bentley', 'Bentley'),
+    ('bugatti', 'Bugatti'),
+    ('ferrari', 'Ferrari'),
+    ('lamborghini', 'Lamborghini'),
+    ('honda', 'Honda'),
+    ('toyota', 'Toyota'),
+    ('chevrolet', 'Chevrolet'),
+    ('porsche', 'Porsche'),
+    ("golf", "Golf"),
+    ("lexus", "Lexus")
+)
+
+TRANSMISSION = (
+    ("manuel", "Manual"),
+    ("automatic", "Automatic"),
+    ("semi-manual", "Semi-Manual")
+)
--- AutomaxApp/models.py
+++ AutomaxApp/models.py
@@ -1,6 +1,8 @@
 import uuid
 from django.db import models
-from userapp . models import Profile
+from userapp . models import Profile, Location
+from . import constants
+from . import utils
 
 # Create your models here.
 class Listing(models.Model):
@@ -8,3 +10,13 @@ class Listing(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now_add=True)
     seller = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    brand = models.CharField(max_length=50, default=None, choices=constants.CAR_BRANDS)
+    model = models.CharField(max_length=150, default=None)
+    chassis_number = models.CharField(max_length=50, default=None)
+    mileage = models.IntegerField(default=0)
+    color = models.CharField(max_length=50, default="White")
+    description = models.TextField(default=None)
+    engine = models.CharField(max_length=50, default=None)
+    transmission = models.CharField(max_length=50, default=None, choices=constants.TRANSMISSION)
+    location = models.OneToOneField(Location, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(upload_to=utils.user_listing_path, max_length=None, default=None)
