f942f70d5e360d31ebbd5dc4e72b9d7f03c3f55a
Wilfred Samuel <wilfredsamuel000@gmail.com>
2023-12-19 10:23:07 +0100

    Adding Widgets

--- userapp/forms.py
+++ userapp/forms.py
@@ -1,8 +1,25 @@
 from django import forms
-from .models import Location
+from .models import Location, Profile
+from .widgets import CustomPictureImageFieldWidget
+from django.contrib.auth.models import User
 
 class LocationForm(forms.ModelForm):
 
     class Meta:
         model = Location
         fields = {"state", "city", "zip_code", "address"}
+
+class ProfileForm(forms.ModelForm):
+    photo = forms.ImageField(widget=CustomPictureImageFieldWidget)
+    bio = forms.TextInput()
+
+    class Meta:
+        model = Profile
+        fields = {"profile_image", "bio", "phone_number"}
+
+class UserForm(forms.ModelForm):
+    username = forms.CharField(disabled=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
